1cb32c49c97b0d07c5505dfa40f9b6c078267e92
Lpinheirocl <lucas.pinheiro1014@gmail.com>
2022-09-12 09:05:14 -0300

    fix: Depositar

--- main.go
+++ main.go
@@ -18,11 +18,23 @@ func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
 		return "Saldo insuficiente"
 	}
 }
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "deposito feito",
+			c.saldo
+	} else {
+		return "deposito insuficiete",
+			c.saldo
+	}
+}
 func main() {
-	contaDaSilvia := ContaCorrente{}
-	contaDaSilvia.titular = "Silvia"
-	contaDaSilvia.saldo = 500
+	contaDaSilvia := ContaCorrente{titular: "Silvia", saldo: 300}
+	contaDoGustavo := ContaCorrente{titular: "Gustavo", saldo: 300}
 
-	fmt.Println(contaDaSilvia)
-	fmt.Println(contaDaSilvia.Sacar(300))
+	fmt.Println(contaDaSilvia.saldo)
+	fmt.Println(contaDoGustavo.saldo)
+	status, valor := contaDaSilvia.Depositar(2000)
+	fmt.Println(status)
+	fmt.Println(valor)
 }
